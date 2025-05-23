d9172440fc24b40a494125d966731a94f4c94d2e
zisis912 <gtf2688@gmail.com>
2025-05-23 21:15:24 +0300

    remove old versions

--- src/main/java/virtuoel/no_fog/util/VersionUtils.java
+++ src/main/java/virtuoel/no_fog/util/VersionUtils.java
@@ -18,14 +18,7 @@ public class VersionUtils
 	{
 		final int[][] ranges =
 		{
-			{1, 14, 4},
-			{1, 15, 2},
-			{1, 16, 5},
-			{1, 17, 1},
-			{1, 18, 2},
-			{1, 19, 4},
-			{1, 20, 5},
-			{1, 21, 0},
+			{1, 21, 4},
 		};
 		
 		final String prefix = ".compat%s%s";
